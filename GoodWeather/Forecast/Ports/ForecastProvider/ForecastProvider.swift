a1797b13784789d3fa50e50fd1c7010e62a29b61
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2021-07-01 11:17:46 +0200

    zz

--- GoodWeather/Forecast/Ports/ForecastProvider/ForecastProvider.swift
+++ GoodWeather/Forecast/Ports/ForecastProvider/ForecastProvider.swift
@@ -1,6 +1,8 @@
+import Combine
+
 protocol ForecastProvider {
     
-    func getForecast(for city: String, callback: @escaping (Result<Forecast, ForecastProviderError>) -> ())
+    func getForecast(for city: String) -> AnyP, callback: @escaping (Result<Forecast, ForecastProviderError>) -> ())
     
     func getForecast(for location: (Double, Double), callback: @escaping (Result<Forecast, ForecastProviderError>) -> ())
     
